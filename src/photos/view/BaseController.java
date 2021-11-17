26aec7275cc6554361450ff2382f0893a4f7b74f
Max Geiger <max.geiger@rutgers.edu>
2021-11-17 13:19:27 -0500

    implemented slide show functionality

--- src/photos/view/BaseController.java
+++ src/photos/view/BaseController.java
@@ -284,4 +284,27 @@ public abstract class BaseController {
         stage.setScene(scene);
         stage.show();
     }
+    
+    /**
+     * Album functions
+     * Open the copy photo scene
+     * @param e user presses button
+     * @param username username of the user
+     * @param albumName name of the album in which the photo resides
+     * @param path path of the image
+     */
+    public void switchToSlideShow(final ActionEvent e, final String username, final String albumName) throws IOException {
+        final FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/photos/view/SlideShow.fxml"));
+        final Parent root = loader.load();
+
+        final SlideShowController controller = loader.getController();
+
+        controller.setField(username, albumName);
+
+        final Scene scene = new Scene(root);
+        final Stage stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
---
+++ src/photos/view/SlideShowController.java
@@ -0,0 +1,118 @@
+package photos.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.*;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import photos.model.User;
+import photos.model.Album;
+import photos.model.Photo;
+
+/**
+ * Controller class to handle slide show
+ * @author Apurva Narde
+ * @author Max Geiger
+ */
+public class SlideShowController extends BaseController{
+
+	/**
+	 * button to proceed to next image
+	 */
+	@FXML Button nextB;
+	
+	/**
+	 * button to proceed to next image
+	 */
+	@FXML Button prevB;
+	
+	/**
+	 * ImageView displaying the image
+	 */
+	@FXML ImageView imageview;
+	
+	/**
+	 * 
+	 * iterator of photos being displayed
+	private ListIterator<Photo> album;
+	*/
+	/**
+	 * ArrayList of photos being displayed
+	 */
+	private ObservableList<Photo> album;
+	/**
+	 * current index in the album
+	 */
+	private int index;
+	
+	/**
+	 * Context variable to contain current Album
+	 */
+	private Album a;
+
+	/**
+	 * Context variable to contain current User
+	 */
+	private User user;
+	
+	/**
+	 * inject data into controller and initialize
+	 */
+	public void setField(String username,String albumname){
+		try {
+			this.user = User.readUser(username);
+		}catch(Exception e) {}
+		this.a = user.getAlbum(albumname);
+		album = a.getPhotos();//album = a.getPhotos().listIterator();
+		index = 0;
+		try {
+			Image im = new Image(new FileInputStream(album.get(index).getPath()));
+			imageview.setImage(im);
+		}catch(Exception ex) {}
+		setButtons();
+	}
+	
+	/**
+	 * checks if next or previous images are available and sets buttons to be deactivated if not
+	 */
+	private void setButtons(){
+		prevB.setDisable(index == 0);
+		nextB.setDisable(index == album.size()-1);
+	}
+
+	/**
+	 * switch to previous image
+	 * @param e user presses button
+	 */
+	public void previousImage(final ActionEvent e) {
+		try {
+			Image im = new Image(new FileInputStream(album.get(--index).getPath()));
+			imageview.setImage(im);
+		}catch(Exception ex) {}
+		setButtons();
+	}
+	
+	/**
+	 * switch to next image
+	 * @param e user presses button
+	 */
+	public void nextImage(final ActionEvent e) {
+		try {
+			Image im = new Image(new FileInputStream(album.get(++index).getPath()));
+			imageview.setImage(im);
+		}catch(Exception ex) {}
+		setButtons();
+	}
+
+	/**
+	 * Switch to the selected album
+	 * @param e user presses button
+	 */
+	public void switchToAlbum(final ActionEvent e) throws IOException {
+		switchToAlbum(e, user.getName(), a.getName(), null);
+	}
+}
